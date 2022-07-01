a058df26e7af1dd03a78b5cb3888347c1f2aa654
Connor Marler <connormarler536@gmail.com>
2022-06-30 19:35:13 -0500

    first commit

---
+++ src/PlayerCharacter.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace DungeonsAndDragons
+{
+  public class PlayerCharacter
+  {
+    //member vars
+    public string? m_PlayerName{get;set;}
+    public string? m_PlayerAge{get;set;}
+
+  }
+
+}
